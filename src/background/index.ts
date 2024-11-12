d8be24b80fe36a9bc1723db733fe2d9f3188002c
Kenechi Okoye <okoyekk@gmail.com>
2024-11-11 22:48:46 -0500

    Added prettier format rules and revised tsconfig

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,36 +1,36 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("BetterTabTool installed");
-});
+    console.log('BetterTabTool installed')
+})
 
 // Copy the current tab's URL to clipboard
 chrome.commands.onCommand.addListener(async (command) => {
-    if (command === "copy-current-tab-url") {
+    if (command === 'copy-current-tab-url') {
         try {
             // Get current active tab in the current window
-            const [tab] = await chrome.tabs.query({
+            const [tab] = (await chrome.tabs.query({
                 active: true,
-                currentWindow: true
-            }) as [chrome.tabs.Tab];
+                currentWindow: true,
+            })) as [chrome.tabs.Tab]
 
             if (!tab?.url || !tab.id) {
-                console.error("No URL found for current tab");
-                return;
+                console.error('No URL found for current tab')
+                return
             }
 
             // Copy URL to clipboard via scripting
             await chrome.scripting.executeScript({
                 target: { tabId: tab.id },
                 func: (url) => {
-                    navigator.clipboard.writeText(url);
+                    navigator.clipboard.writeText(url)
                 },
-                args: [tab.url]
-            });
+                args: [tab.url],
+            })
 
-            console.log("URL copied to clipboard");
+            console.log('URL copied to clipboard')
 
             // TODO: Show a notification via popup
         } catch (err) {
-            console.error("Error copying URL to clipboard: ", err);
+            console.error('Error copying URL to clipboard: ', err)
         }
     }
-});
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -10,36 +10,36 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+                exclude: /node_modules/,
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/popup/index.html',
-            filename: 'popup.html'
+            filename: 'popup.html',
         }),
         new CopyWebpackPlugin({
             patterns: [
-                {from: 'manifest.json', to: 'manifest.json'} // Copy manifest from top level to dist/
-            ]
-        })
+                { from: 'manifest.json', to: 'manifest.json' }, // Copy manifest from top level to dist/
+            ],
+        }),
     ],
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
         },
         compress: true,
-        port: 9000
-    }
-};
+        port: 9000,
+    },
+}
