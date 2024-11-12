ded15439997e51ded8251bd1005658e73a1c5bb7
Kenechi Okoye <okoyekk@gmail.com>
2024-11-11 22:33:44 -0500

    Updated target JS version and got Copy Current Tab to work

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,10 +2,35 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("BetterTabTool installed");
 });
 
-
-// Copy current tab's URL to clipboard
-chrome.commands.onCommand.addListener((command) => {
+// Copy the current tab's URL to clipboard
+chrome.commands.onCommand.addListener(async (command) => {
     if (command === "copy-current-tab-url") {
-        console.log("TODO: Copying current tab's URL to clipboard");
+        try {
+            // Get current active tab in the current window
+            const [tab] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true
+            }) as [chrome.tabs.Tab];
+
+            if (!tab?.url || !tab.id) {
+                console.error("No URL found for current tab");
+                return;
+            }
+
+            // Copy URL to clipboard via scripting
+            await chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: (url) => {
+                    navigator.clipboard.writeText(url);
+                },
+                args: [tab.url]
+            });
+
+            console.log("URL copied to clipboard");
+
+            // TODO: Show a notification via popup
+        } catch (err) {
+            console.error("Error copying URL to clipboard: ", err);
+        }
     }
-})
+});
